306477982f6c4eb560620aecbe5c72e10174fcd3
Clawber <jssarenas@up.edu.ph>
2022-11-22 00:20:13 +0800

    Initial commit
    Site displays a map from leaflet.js

---
+++ script.js
@@ -0,0 +1,13 @@
+let mapOptions = {
+  center:[51.958, 9.141],
+  zoom:10
+}
+
+
+let map = new L.map('map' , mapOptions);
+
+let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+map.addLayer(layer);
+
+let marker = new L.Marker([51.958, 9.141]);
+marker.addTo(map);
