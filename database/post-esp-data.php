6bf21a4859ff456a3d01e19473cb9e1fce1dbabd
Yenzy Hebron <yshebron@up.edu.ph>
2023-03-14 18:33:42 +0800

    Did npm install pg for node-pg API. Added starter file node-pg.js. In progress: Converting post-esp-data.php from MySQL to PSQL connect.

--- database/post-esp-data.php
+++ database/post-esp-data.php
@@ -1,108 +1,121 @@
 <?php
-// Arduino Model: ESP8266 NodeMCU
-$servername = "localhost";
-$username = "root";
-$password = "jeeps";
-$dbname = "sensordata";
+    // Arduino Model: ESP8266 NodeMCU
+    // $servername = "localhost";
+    // $username = "root";
+    // $password = "jeeps";
+    // $dbname = "sensordata";
 
-/*$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);*/
+    $host        = "host=192.168.100.124"; # Phone WiFi hotspot (ipconfig)
+    $port        = "port=5432";
+    $dbname      = "dbname=jeeps";
+    $credentials = "user=postgres password=jeeps101";
 
-$api_key_value = "tPmAT5Ab3j7F9";
+    /*$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);*/
 
-// Initialize vars to empty string
-$api_key = $jeepID = $X = $Y = $PlateNumber = $Time = $Date = "";
+    $api_key_value = "tPmAT5Ab3j7F9";
 
-// $_SERVER: superglobal, holds info about headers, paths, and script locations.
-// "REQUEST METHOD": Returns request method used to access the page (such as POST)
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $api_key = test_input($_POST["api_key"]);
-    if($api_key == $api_key_value) {
-        $JeepID = $_POST["ID"];
-        $X = $_POST["X"];
-        $Y = $_POST["Y"];
-        //$Date = $_POST["Date"];
-        //$Time = $_POST["Time"];
-        //$PlateNumber = $_POST["Plate"];
+    // Initialize vars to empty string
+    $api_key = $jeepID = $X = $Y = $PlateNumber = $Time = $Date = "";
 
-        // Create connection
-        $conn = new mysqli($servername, $username, $password, $dbname);
+    // $_SERVER: superglobal, holds info about headers, paths, and script locations.
+    // "REQUEST METHOD": Returns request method used to access the page (such as POST)
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $api_key = test_input($_POST["api_key"]);
+        if($api_key == $api_key_value) {
+            $JeepID = $_POST["ID"];
+            $X = $_POST["X"];
+            $Y = $_POST["Y"];
+            //$Date = $_POST["Date"];
+            //$Time = $_POST["Time"];
+            //$PlateNumber = $_POST["Plate"];
 
-        // Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        } 
+            // Create connection (converted from MySQL to PSQL)
+            // $conn = new mysqli($servername, $username, $password, $dbname);
+            if ($db = pg_connect("$host $port $dbname $credentials")) {
+                echo "Connected successfully to $dbname";
+            } else {
+                echo "Error: Connection failure";
+            }
+
+            // Print extra data about
+
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            } 
+            
+            /*$sql = "INSERT INTO jeepinfo SET Coordinates = '$Coordinates' WHERE JeepID = 'jeepID'";*/
+            /*$sql = "INSERT INTO jeepinfo (JeepID, X, Y, Date, Time, PlateNumber)
+            VALUES ('" . $JeepID . "', '" . $X . "', '" . $Y . "', '" . $Date . "', '" . $Time . "', '" . $PlateNumber . "')";*/
+            /*UPDATE `jeepinfo` SET X = 0.00, Y = 0.00 WHERE JeepID = 1;*/
+            $sql = "UPDATE `jeepinfo` SET X = $X, Y = $Y, Time = now(), Date = CURDATE() WHERE JeepID = $JeepID";
+            //http://localhost/sensordata/post-esp-data.php?api_key=tPmAT5Ab3j7F9&ID=0&X=12.3&Y=18.4
+            if ($conn->query($sql) === TRUE) {
+                echo "Record updated successfully";
+            } 
+            else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
         
-        /*$sql = "INSERT INTO jeepinfo SET Coordinates = '$Coordinates' WHERE JeepID = 'jeepID'";*/
-        /*$sql = "INSERT INTO jeepinfo (JeepID, X, Y, Date, Time, PlateNumber)
-        VALUES ('" . $JeepID . "', '" . $X . "', '" . $Y . "', '" . $Date . "', '" . $Time . "', '" . $PlateNumber . "')";*/
-	    /*UPDATE `jeepinfo` SET X = 0.00, Y = 0.00 WHERE JeepID = 1;*/
-	    $sql = "UPDATE `jeepinfo` SET X = $X, Y = $Y, Time = now(), Date = CURDATE() WHERE JeepID = $JeepID";
-        //http://localhost/sensordata/post-esp-data.php?api_key=tPmAT5Ab3j7F9&ID=0&X=12.3&Y=18.4
-        if ($conn->query($sql) === TRUE) {
-            echo "Record updated successfully";
-        } 
-        else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
+            $conn->close();
         }
-    
-        $conn->close();
+        else {
+            echo "Wrong API Key provided.";
+        }
+
+    }else if ($_SERVER["REQUEST_METHOD"] == "GET") {
+        $api_key = test_input($_POST["api_key"]);
+        if($api_key == $api_key_value) {
+            $JeepID = $_GET["ID"];
+            $X = $_GET["X"];
+        $Y = $_GET["Y"];
+        //$Date = $_GET["Date"];
+        //$Time = $_GET["Time"];
+        //$PlateNumber = $_GET["Plate"];
+
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            } 
+            
+            /*$sql = "INSERT INTO jeepinfo SET Coordinates = '$Coordinates' WHERE JeepID = 'jeepID'";*/
+            /*$sql = "INSERT INTO jeepinfo (JeepID, X, Y, Date, Time, PlateNumber)
+            VALUES ('" . $JeepID . "', '" . $X . "', '" . $Y . "', '" . $Date . "', '" . $Time . "', '" . $PlateNumber . "')";*/
+        /*UPDATE `jeepinfo` SET X = 0.00, Y = 0.00 WHERE JeepID = 1;*/
+        $sql = "UPDATE `jeepinfo` SET X = $X, Y = $Y, Time = now(), Date = CURDATE() WHERE JeepID = $JeepID";
+            //http://localhost/sensordata/post-esp-data.php?api_key=tPmAT5Ab3j7F9&ID=0&X=12.3&Y=18.4
+            if ($conn->query($sql) === TRUE) {
+                echo "Record updated successfully";
+            } 
+            else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+        
+            $conn->close();
+        }
+        else {
+            echo "Wrong API Key provided.";
+        }
+
     }
     else {
-        echo "Wrong API Key provided.";
+        echo "No data posted with HTTP POST.";
     }
 
-}else if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    $api_key = test_input($_POST["api_key"]);
-    if($api_key == $api_key_value) {
-        $JeepID = $_GET["ID"];
-        $X = $_GET["X"];
-	$Y = $_GET["Y"];
-	//$Date = $_GET["Date"];
-	//$Time = $_GET["Time"];
-	//$PlateNumber = $_GET["Plate"];
-
-        // Create connection
-        $conn = new mysqli($servername, $username, $password, $dbname);
-
-        // Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        } 
-        
-        /*$sql = "INSERT INTO jeepinfo SET Coordinates = '$Coordinates' WHERE JeepID = 'jeepID'";*/
-        /*$sql = "INSERT INTO jeepinfo (JeepID, X, Y, Date, Time, PlateNumber)
-        VALUES ('" . $JeepID . "', '" . $X . "', '" . $Y . "', '" . $Date . "', '" . $Time . "', '" . $PlateNumber . "')";*/
-	/*UPDATE `jeepinfo` SET X = 0.00, Y = 0.00 WHERE JeepID = 1;*/
-	$sql = "UPDATE `jeepinfo` SET X = $X, Y = $Y, Time = now(), Date = CURDATE() WHERE JeepID = $JeepID";
-        //http://localhost/sensordata/post-esp-data.php?api_key=tPmAT5Ab3j7F9&ID=0&X=12.3&Y=18.4
-        if ($conn->query($sql) === TRUE) {
-            echo "Record updated successfully";
-        } 
-        else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-    
-        $conn->close();
-    }
-    else {
-        echo "Wrong API Key provided.";
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
     }
 
-}
-else {
-    echo "No data posted with HTTP POST.";
-}
-
-function test_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}
-
-// Notes (cleanup before finalization:)
-/*The name attribute is for submitting a form element to the server;
-many elements may share the same name (e.g. radio buttons, which must
-have the same name within the set). The id attribute is for uniquely
-identifying any element (not just form elements). It must be unique
-throughout the entire document.*/
+    // Notes (cleanup before finalization:)
+    /*The name attribute is for submitting a form element to the server;
+    many elements may share the same name (e.g. radio buttons, which must
+    have the same name within the set). The id attribute is for uniquely
+    identifying any element (not just form elements). It must be unique
+    throughout the entire document.*/
+?>
