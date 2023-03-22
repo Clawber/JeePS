f8a9c1e15b4fe666834e7fa14a06daf8cf84f083
Yenzy Hebron <yshebron@up.edu.ph>
2023-03-22 15:14:27 +0800

    Still working on JeePS schema and dummy, pushing for Arduino code.

--- database/jeeps-schema.sql
+++ database/jeeps-schema.sql
@@ -1,15 +1,16 @@
 -- Backticks are not supported by psql
+-- Added driverID that references ID at driver table
 CREATE TABLE jeepney (
 	ID			SERIAL (6) NOT NULL PRIMARY KEY,
 	tracker		SMALLINT NOT NULL REFERENCES tracker(ID),
 	route		VARCHAR(3) NOT NULL REFERENCES route(name),
 	plateNumber	VARCHAR(6) NOT NULL,
-	driverFName	VARCHAR(20) NOT NULL,
-	driverLName	VARCHAR(20) NOT NULL,
 	capacity	SMALLINT NOT NULL,
 	UNIQUE(tracker, route, plateNumber)
 );
 
+-- Removed battery from tracker.
+-- POINT data type requires '' when being encoded.
 CREATE TABLE tracker (
 	ID			SMALLINT NOT NULL PRIMARY KEY,
 	coords		POINT
@@ -22,3 +23,9 @@ CREATE TABLE route (
 	color		INT NOT NULL,
 	path		PATH NOT NULL
 );
+
+CREATE TABLE driver (
+	ID			SERIAL (6)  NOT NULL PRIMARY KEY
+	driverFName	VARCHAR(20) NOT NULL,
+	driverLName	VARCHAR(20) NOT NULL	
+)
--- database/node-pg.js
+++ database/node-pg.js
@@ -1,7 +1,20 @@
-const { Client } = require('pg')
-const client = new Client()
-await client.connect()
- 
-const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+const { Client } = require('pg');
+const client = new Client({
+    host: "localhost",
+    user: "postgres",
+    port: "5432",
+    password: "jeeps101",
+    database: "jeeps"
+});
+
+await client.connect();
+
+const res = await client.query('SELECT * FROM tracker, (err, res) => {
+    if (!err) {
+        console.log(res,rows);
+    } else {
+        console.log(err,message);
+    }
+    await client.end()
+});
 console.log(res.rows[0].message) // Hello world!
-await client.end()
