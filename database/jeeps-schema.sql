b8819738362c99598a1dfcc80fe6ca15b05c82cd
Yenzy Hebron <yshebron@up.edu.ph>
2023-03-22 15:38:13 +0800

    Updated PSQL scshema. Added driver table.

--- database/jeeps-schema.sql
+++ database/jeeps-schema.sql
@@ -1,31 +1,34 @@
--- Backticks are not supported by psql
--- Added driverID that references ID at driver table
+-- Backticks are not supported by psql.
+-- Added driverID that references ID at driver table.
 CREATE TABLE jeepney (
-	ID			SERIAL (6) NOT NULL PRIMARY KEY,
-	tracker		SMALLINT NOT NULL REFERENCES tracker(ID),
-	route		VARCHAR(3) NOT NULL REFERENCES route(name),
+	ID			SERIAL NOT NULL PRIMARY KEY,
+	trackerID	SERIAL NOT NULL REFERENCES tracker(ID),
+	routeID		SERIAL NOT NULL REFERENCES route(ID),
+	driverID	SERIAL NOT NULL REFERENCES driver(ID),
 	plateNumber	VARCHAR(6) NOT NULL,
 	capacity	SMALLINT NOT NULL,
-	UNIQUE(tracker, route, plateNumber)
+	UNIQUE(trackerID, routeID, driverID, plateNumber)
 );
 
 -- Removed battery from tracker.
 -- POINT data type requires '' when being encoded.
 CREATE TABLE tracker (
-	ID			SMALLINT NOT NULL PRIMARY KEY,
+	ID			SERIAL NOT NULL PRIMARY KEY,
 	coords		POINT
 );
 
 -- Color is stored as INT, can be converted using HEX()
 -- Path can be open () or closed []
 CREATE TABLE route (
-	name		VARCHAR(20) NOT NULL PRIMARY KEY,
-	color		INT NOT NULL,
-	path		PATH NOT NULL
+	ID			SERIAL NOT NULL PRIMARY KEY,
+	name		VARCHAR(20) NOT NULL,
+	color		INT NOT NULL,			-- Color is encoded as integer.
+	path		PATH NOT NULL,
+	UNIQUE(name)
 );
 
 CREATE TABLE driver (
-	ID			SERIAL (6)  NOT NULL PRIMARY KEY
+	ID			SERIAL NOT NULL PRIMARY KEY,
 	driverFName	VARCHAR(20) NOT NULL,
-	driverLName	VARCHAR(20) NOT NULL	
+	driverLName	VARCHAR(20) NOT NULL
 )
