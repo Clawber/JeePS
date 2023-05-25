65344d9040879c64e08497af45ad980fe0450ea4
Yenzy Hebron <yshebron@up.edu.ph>
2023-05-25 21:53:46 +0800

    Untracked .netlify. Need to review package.json.

--- .netlify/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-export const component = async () => (await import('../entries/pages/_layout.svelte.js')).default;
-export const file = '_app/immutable/entry/_layout.svelte.ec98e287.js';
-export const imports = ["_app/immutable/entry/_layout.svelte.ec98e287.js","_app/immutable/chunks/index.6a3274fc.js","_app/immutable/chunks/stores.dcd146ec.js","_app/immutable/chunks/singletons.7aceefbf.js"];
-export const stylesheets = ["_app/immutable/assets/_layout.1258868b.css"];
-export const fonts = [];
--- .netlify/server/nodes/4.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 4;
-export const component = async () => (await import('../entries/pages/faqPage/_page.svelte.js')).default;
-export const file = '_app/immutable/entry/faqPage-page.svelte.be0e9518.js';
-export const imports = ["_app/immutable/entry/faqPage-page.svelte.be0e9518.js","_app/immutable/chunks/index.6a3274fc.js"];
-export const stylesheets = [];
-export const fonts = [];
