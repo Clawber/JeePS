fab23f68a9822e35f1b7f56ca28515f048130a4c
Yenzy Hebron <yshebron@up.edu.ph>
2023-05-25 15:37:13 +0800

    Renamed userName to username. Removed host property in sequelize.

--- backend/models/index.js
+++ backend/models/index.js
@@ -5,8 +5,7 @@ const { Pool } = require('pg');
 // (change this to internal conn once deployed on Render)
 const connection = 'postgres://admin:FWq5lKmt9n8rGtyDZoUPGuTkrR8XM7v6@dpg-cgtqnlt269vbmevdbd9g-a.singapore-postgres.render.com:5432/jeeps?ssl=true';
 const sequelize = new Sequelize(connection, {
-    dialect: "postgres",
-    host: 'localhost'
+    dialect: "postgres"
 });
 
 // Checking if connection is done
@@ -22,7 +21,6 @@ db.sequelize = sequelize;
 
 //connecting to model
 db.users = require('./userModel') (sequelize, DataTypes)
-db.sequelize.sync();
 
 // NOTE: Reuse DB connection with other stuff
 const pool = new Pool({connectionString: connection})
