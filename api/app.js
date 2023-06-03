71b3dc6b48d6e85c33b57a5718374b670fb8efbd
Yenzy Hebron <yshebron@up.edu.ph>
2023-06-04 02:39:30 +0800

    CRUD now working! Some minor changes in app.js.

--- api/app.js
+++ api/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const { db, pool } = require('./models');
+const { db } = require('./models');
 const userRoutes = require('./routes/userRoutes');
 const jeepsRoutes = require('./routes/jeepsRoutes');
 const cors = require('cors');
@@ -17,15 +17,15 @@ const csvSync = require('./controllers/dbController')
 const [Driver, Jeepney, Route] = [db.Driver, db.Jeepney, db.Route];
 // const filepaths = ['db/driver.csv', 'db/route.csv', 'db/tracker.csv', 'db/jeepney.csv']
 
-Driver.sync({force:true}).then(() => {
+Driver.sync().then(() => {
     console.log('Driver has been synced.');
     csvSync(Driver, 'db/driver.csv')
 }).then(() => {
-    Route.sync({force:true}).then(() => {
+    Route.sync().then(() => {
         console.log('Route has been synced.');
         csvSync(Route, 'db/route.csv');
     }).then(() => {
-        Jeepney.sync({force:true}).then(() => {
+        Jeepney.sync().then(() => {
             console.log('Jeepney has been synced.');
             csvSync(Jeepney, 'db/jeepney.csv');
         })
@@ -33,6 +33,8 @@ Driver.sync({force:true}).then(() => {
 })
 
 db.User.sync();
+
+//db.sequelize.sync();
 
 app.use('/api/users', userRoutes)
 app.use('/api/jeeps', jeepsRoutes)
