a339a61251cf73d3cf7501cb81ebcc647646bc72
Yenzy Hebron <yshebron@up.edu.ph>
2023-06-03 18:06:55 +0800

    Refactored app.js, added .seqeulizerc and cli.

--- api/app.js
+++ api/app.js
@@ -1,31 +1,18 @@
-const express = require('express')
-const sequelize = require('sequelize')
-const dotenv = require('dotenv').config()
-const cookieParser = require('cookie-parser')
-const { db, pool } = require('./models')
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const { db, pool } = require('./models');
+const userRoutes = require('./routes/userRoutes');
+const jeepsRoutes = require('./routes/jeepsRoutes');
+const cors = require('cors');
 
-const userRoutes = require('./routes/userRoutes')
-const jeepsRoutes = require('./routes/jeepsRoutes')
-
-const Joi = require('joi')
-const cors = require('cors')
-
-// Setting up our port
 const PORT = process.env.PORT || 8080
 
-// Assigning the variable app to express
-// IDEA: Only use one entry point, app, for our purposes
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-// Middleware
-
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cookieParser())
-
-// TODO: Non-destructive sync of .csv with db. Uses Migrations.
-// Selective sync, then Synchronize .csv files with database
 const csvSync = require('./controllers/dbController')
 const [Driver, Jeepney, Route, Tracker] = [db.driver, db.jeepney, db.route, db.tracker];
 // const filepaths = ['db/driver.csv', 'db/route.csv', 'db/tracker.csv', 'db/jeepney.csv']
@@ -47,9 +34,7 @@ Driver.sync({force:true}).then(() => {
 
 db.users.sync();
 
-// Assign handler modules to URIs
 app.use('/api/users', userRoutes)
 app.use('/api/jeeps', jeepsRoutes)
 
-// Listening to server connection
 app.listen(PORT, () => console.log(`Server is connected on ${PORT}`))
