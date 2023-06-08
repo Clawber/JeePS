53dbbc2d5c0450b2049956ff8cf245b89a32c39e
Yenzy Hebron <yshebron@up.edu.ph>
2023-06-08 17:14:01 +0800

    refactor: now using SECRET_KEY from .env

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,6 +1,6 @@
 //importing modules
 const bcrypt = require("bcrypt");
-const { db, pool } = require("../models");
+const { db } = require("../models");
 const jwt = require("jsonwebtoken");
 require('dotenv').config()
 
@@ -21,14 +21,14 @@ const signup = async (req, res) => {
    const user = await User.create(data);
 
    //if user details is captured
-   //generate token with the user's id and the secretKey in the env file
+   //generate token with the user's id and the SECRET_KEY in the env file
    // set cookie with the token generated
    if (user) {
-     let token = jwt.sign({ id: user.id }, process.env.secretKey, {
-       expiresIn: 1 * 24 * 60 * 60 * 1000,
+     let token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, {
+       expiresIn: 24 * 60 * 60 * 1000,
      });
 
-     res.cookie("jwt", token, { maxAge: 1 * 24 * 60 * 60, httpOnly: true });
+     res.cookie("jwt", token, { maxAge: 24 * 60 * 60, httpOnly: true });
      console.log("user", JSON.stringify(user, null, 2));
      console.log(token);
      //send users details
@@ -61,16 +61,16 @@ const login = async (req, res) => {
       const isSame = await bcrypt.compare(password, user.password);
 
       //if password is the same
-        //generate token with the user's id and the secretKey in the env file
+        //generate token with the user's id and the SECRET_KEY in the env file
 
       if (isSame) {
-        let token = jwt.sign({ id: user.id }, process.env.secretKey, {
-          expiresIn: 1 * 24 * 60 * 60 * 1000,
+        let token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, {
+          expiresIn: 24 * 60 * 60 * 1000,
         });
 
         //if password matches wit the one in the database
         //go ahead and generate a cookie for the user
-        res.cookie("jwt", token, { maxAge: 1 * 24 * 60 * 60, httpOnly: true });
+        res.cookie("jwt", token, { maxAge: 24 * 60 * 60, httpOnly: true });
         console.log("user", JSON.stringify(user, null, 2));
         console.log(token);
         //send user data
