41d5a3cddee0c7a681119f8b7cae0aeccbe9a9bf
Yenzy Hebron <yshebron@up.edu.ph>
2023-06-27 23:47:15 +0800

    feat: added default light / dark mode toggle

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -2,7 +2,6 @@
   import { onMount, onDestroy } from 'svelte';
   import { browser } from '$app/environment';
   import {IKOTRoutePoints, SMPANTRANCORoutePoints, TOKIDAYRoutePoints, TOKINIGHTRoutePoints, PHILCOADAYRoutePoints, PHILCOANIGHTRoutePoints, KATIPUNANDAYRoutePoints, KATIPUNANNIGHTRoutePoints} from './jeepRoutes.js';
-  import jeepMarker from '$lib/images/jeep_marker.png';
 
   const DEV = false;
   // For development, use 'http://localhost:8080/api/jeeps/jeepney'
