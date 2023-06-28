5e0e59670cf36ab9a913a166dcde3c367667f17b
Yenzy Hebron <yshebron@up.edu.ph>
2023-06-28 21:59:51 +0800

    feat: Removed unused parameter `details`

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -7,6 +7,7 @@
   // For development, use 'http://localhost:8080/api/jeeps/jeepney'
   // For production, use 'https://jeeps-alt.onrender.com/api/jeeps/jeepney'
   const GET_ALL_JEEPNEYS_URL = DEV ? 'http://localhost:8080/api/jeeps/jeepney' : 'https://jeeps-alt.onrender.com/api/jeeps/jeepney'
+  const GET_ALL_ROUTES_URL = DEV ? 'http://localhost:8080/api/jeeps/route' : 'https://jeeps-alt.onrender.com/api/jeeps/route'
 
   let mapElement;
   let map;
@@ -24,11 +25,11 @@
       const bounds = L.latLngBounds(southWest, northEast);
 
       // TILELAYERS
-      var tileLight = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      const tileLight = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       });
 
-      var tileDark = L.tileLayer('https://{s}.tile.jawg.io/jawg-dark/{z}/{x}/{y}{r}.png?access-token={accessToken}', {
+      const tileDark = L.tileLayer('https://{s}.tile.jawg.io/jawg-dark/{z}/{x}/{y}{r}.png?access-token={accessToken}', {
         attribution: '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         minZoom: 0,
         maxZoom: 22,
@@ -51,12 +52,14 @@
       console.log("new map created");
       map = L.map(mapElement, mapOptions)
 
-      var tiles = {
+      const tiles = {
         "Light Mode": tileLight,
         "Dark Mode": tileDark
       }
 
-      // Define Jeepney Class (represented by marker on map)
+        var mapControls = L.control.layers(tiles, null, {sortLayers: true}).addTo(map);
+
+        // Define Jeepney Class (represented by marker on map)
       var jeepTag = L.Icon.extend({
         options: {
           iconSize:     [80, 96], // size of the icon
@@ -65,7 +68,9 @@
         }
       });
 
-      var defaultCoords = L.latLng(14.65491, 121.06862);
+      // UP central
+      const defaultCoords = L.latLng(14.65491, 121.06862);
+      const defaultPolyline = [[14.65387, 121.06861], [14.65615, 121.0686]];
 
       class Jeep {
         constructor(map, details) {
@@ -96,12 +101,12 @@
             this.routename = details.Route.name;
 
             this.marker.setLatLng(details.coords ? this.coords : defaultCoords);
-            this.popup();
+            this.popup(details);
         }
 
         popup(details) {
           this.marker.bindPopup(
-            `Jeepney ID: ${this.id} <br>
+            `Jeepney ID: ${this.id}<br>
              Plate Number: ${this.platenumber}<br>
              Capacity: ${this.capacity ? this.capacity : 'Undefined'}<br>
              Driver Name: ${this.drivername? this.drivername : 'Undefined' }<br>
@@ -142,72 +147,76 @@
           setInterval(updateJeeps, 1000 * 3)
         })
       })
-
-      // TODO: Add orientation (arrows) to route
-      class Route {
+        // TODO: Fix layer control name (name - Day/Night)
+        class Route {
           constructor(map, details) {
               this.map = map;
               this.id = details.id;
               this.name = details.name;
               this.color = details.color;
-              this.path = details.path;
+              this.path = details.path ? details.path : defaultPolyline;
 
-              // Map this Jeep to a marker of its own
-              this.marker = new L.Marker(this.coords, {icon: new jeepTag({
-                      iconUrl: this.routename ? `../tags/dark/${this.routename}.png` : `../tags/dark/Ikot.png`})});
-              this.marker.addTo(this.map);
-              this.popup(details);
-          }
+              // Map this Route to a Polyline of its own
+              this.polyline = new L.Polyline(this.path, {name: "test", color: this.color, weight: 5, smoothFactor: 3}).addTo(map);
+              mapControls.addOverlay(this.polyline, this.name);
 
-          set(details) {
-              this.platenumber = details.platenumber;
-              this.capacity = details.capacity;
-              this.coords = details.coords ? L.latLng(details.coords.x, details.coords.y) : L.latLng(14.65491, 121.06862);
-              this.driverid = details.driverid;
-              this.drivername = details.Driver.firstname + ' ' + details.Driver.lastname;
-              this.routeid = details.routeid;
-              this.routename = details.Route.name;
-
-              this.marker.setLatLng(details.coords ? this.coords : L.latLng(14.65491, 121.06862));
               this.popup();
           }
 
-          popup(details) {
-              this.marker.bindPopup(
-                  `Jeepney ID: ${this.id} <br>
-             Plate Number: ${this.platenumber}<br>
-             Capacity: ${this.capacity ? this.capacity : 'Undefined'}<br>
-             Driver Name: ${this.drivername? this.drivername : 'Undefined' }<br>
-             Route Name: ${this.routename ? this.routename : 'Undefined'}<br>
-             Coords: ${details.coords ? `(${this.coords.lat},${this.coords.lng})` : 'Undefined'}<br>`
+          set(details) {
+              this.name = details.name;
+              this.color = details.color;
+              this.path = details.path ? details.path : defaultPolyline;
+
+              this.polyline.setLatLngs(this.path);
+              this.popup();
+          }
+
+          // TODO: Show how many jeepneys are in a route for List
+          popup() {
+              this.polyline.bindPopup(
+                  `Route ID: ${this.id}<br>
+                 Name: ${this.name}<br>
+                 Color: ${this.color}<br>
+                 Jeepneys: ${jeepneys.filter(jeep => jeep.routeid === this.id).length}<br>`
               );
           }
       }
-      // Function addRoutes
-      function addRoutes(map) {
-        var IKOTRoute = L.polyline(IKOTRoutePoints, {color: '#ffcd32', weight: 5, smoothFactor: 3}).addTo(map);
-        var SMPANTRANCORoute = L.polyline(SMPANTRANCORoutePoints, {color: '#329a9a', weight: 5, smoothFactor: 3})
-        var TOKIDAYRoute = L.polyline(TOKIDAYRoutePoints, {color: '#f68c34', weight: 5, smoothFactor: 3})
-        var TOKINIGHTRoute = L.polyline(TOKINIGHTRoutePoints, {color: '#88202c', weight: 5, smoothFactor: 3})
-        var PHILCOADAYRoute = L.polyline(PHILCOADAYRoutePoints, {color: '#98cd67', weight: 5, smoothFactor: 3})
-        var PHILCOANIGHTRoute = L.polyline(PHILCOANIGHTRoutePoints, {color: '#0f6752', weight: 5, smoothFactor: 3})
-        var KATIPUNANDAYRoute = L.polyline(KATIPUNANDAYRoutePoints, {color: '#e42f59', weight: 5, smoothFactor: 3})
-        var KATIPUNANNIGHTRoute = L.polyline(KATIPUNANNIGHTRoutePoints, {color: '#663367', weight: 5, smoothFactor: 3})
 
-        var jeepRoutes = {
-          "Ikot" : IKOTRoute,
-          "SM North - Pantranco" : SMPANTRANCORoute,
-          "Toki - Day" : TOKIDAYRoute,
-          "Toki - Night" : TOKINIGHTRoute,
-          "Philcoa - Day" : PHILCOADAYRoute,
-          "Philcoa - Night" : PHILCOANIGHTRoute,
-          "Katipunan - Day" : KATIPUNANDAYRoute,
-          "Katipunan - Night" : KATIPUNANNIGHTRoute,
+        var routes = [];
+
+        function updateRoutes() {
+        fetch(GET_ALL_ROUTES_URL).then((res) => {
+            res.json().then(async (data) => {
+                data.ret.forEach((route) => {
+                    let found = routes.find(elem => elem.id === route.id);
+                    if (found === undefined) {
+                        routes.push(new Route(map, route));
+                    } else {
+                        found.set(route);
+                    }
+                })
+            })
+        })
         }
-        L.control.layers(tiles, jeepRoutes).addTo(map);
-      }
 
-      addRoutes(map);
+        // Fetch all routes and instantiate each routes
+        // API returns array of routes
+        // TODO: Fix sortFunction
+        await fetch(GET_ALL_ROUTES_URL).then((res) => {
+        console.log(res)
+        res.json().then((data) => {
+            console.log(data);
+            data.ret.forEach((route) => {
+                routes.push(new Route(map, route));
+            })
+        }).then(() => {
+            console.log(routes);
+            setInterval(updateRoutes, 1000 * 10)
+        })
+        })
+
+
 
       function updateIconMode() {
           if (map.hasLayer(tileLight)) {
