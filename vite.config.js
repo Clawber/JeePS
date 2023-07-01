5800772f4b3d3fca9fc62e2cef0de11de6b751ab
Yenzy Hebron <yshebron@up.edu.ph>
2023-07-01 17:53:32 +0800

    test: dropped console and debugger (only) on production for security

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	esbuild: {
+		drop: ['console', 'debugger'],
+	},
 	build: {
 		rollupOptions: {
 			plugins: [
